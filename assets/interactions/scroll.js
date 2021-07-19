f7e81e848588d2237baa5ae83402f61208435d6b
Jiatong Zhong <jiatongz@kth.se>
2021-07-19 16:56:15 +0200

    homepage adjustment

---
+++ assets/interactions/scroll.js
@@ -0,0 +1,27 @@
+// var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+var rootElement = document.documentElement;
+
+function scrollToTop() {
+    var rootElement = document.documentElement;
+    // Scroll to top logic
+    rootElement.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+  }
+
+  // window.onload = function() {
+  //   $('#scrollToTop').hide();
+  // }
+
+  $(document).scroll(function () {
+    if(window.scrollY > 100)
+    {
+      $('#bounce-arrow').hide();
+      $('#scrollToTopBtn').show();
+    }
+    else{
+      $('#bounce-arrow').show();
+      $('#scrollToTopBtn').hide();
+    }
+  });
